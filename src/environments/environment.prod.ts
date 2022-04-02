92fbcf88be4a64666914649707ae5bbb0c898fa1
ujjal bhaskar <ujjal.bhaskar@mheducation.com>
2022-04-02 13:37:21 +0530

    Revert to default master

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,15 +2,17 @@ export const environment = {
   production: true,
   envName: "production",
   firebaseConfig: {
-    apiKey: "AIzaSyDE1Z6ilF6tkrSVR4VRtTLlU8lKzP9gEmU",
-    authDomain: "letscng-prod.firebaseapp.com",
-    projectId: "letscng-prod",
-    storageBucket: "letscng-prod.appspot.com",
-    messagingSenderId: "245254811651",
-    appId: "1:245254811651:web:5248fd0542728f48961e36",
+    apiKey: "AIzaSyBd9AIpEXU-jfkp40JlCi0N34QiqHS7wW0",
+    authDomain: "challenge1177.firebaseapp.com",
+    databaseURL: "https://challenge1177-default-rtdb.firebaseio.com",
+    projectId: "challenge1177",
+    storageBucket: "challenge1177.appspot.com",
+    messagingSenderId: "59705937323",
+    appId: "1:59705937323:web:73d7ba7d7776d4f58a16ee",
+    measurementId: "G-CZ87RT5HH8"
   },
   stravaConfig: {
-    client_id: 79860,
+    client_id: 69720,
   },
   backend: {
     apiBaseUrl: "https://letscng-backend-bsx6kqq36q-uc.a.run.app",

7fab15d400d2c7674b180a24fe71b948f1284552
ujjal bhaskar <ujjal.bhaskar@mheducation.com>
2022-04-17 21:14:19 +0530

    Adding Prateek as Admin

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -18,5 +18,5 @@ export const environment = {
     apiBaseUrl: "https://letscng-backend-bsx6kqq36q-uc.a.run.app",
     cng_redirect_uri: "https://letcng-prod.web.app/strava",
   },
-  admins: ['9831035620', '9748437466'],
+  admins: ['9831035620', '9748437466', '9903699009'],
 };

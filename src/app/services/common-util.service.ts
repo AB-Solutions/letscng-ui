c6e59f31141ec2ac4766ffb151616d40b88c4a96
ujjal bhaskar <ujjal.bhaskar@mheducation.com>
2022-06-22 13:02:06 +0530

    Aw80d 2022

--- src/app/services/common-util.service.ts
+++ src/app/services/common-util.service.ts
@@ -6,7 +6,7 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root'
 })
 export class CommonUtilService {
-  maintenance = true;
+  maintenance = false;
   loadingMessage = new EventEmitter<string>();
   loadingSuccessMessage = new EventEmitter<string>();
   loadSelectedTeamList = new EventEmitter<string>();

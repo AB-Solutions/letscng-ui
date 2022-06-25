bd156f41a312bedde2484da7b3d8e25eada92821
ujjal bhaskar <ujjal.bhaskar@mheducation.com>
2022-06-25 15:21:08 +0530

    publish 1.0.1

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Chart } from 'chart.js';
 import { CommonUtilService } from '../services/common-util.service';
-
+const pack = require('../../../package.json');
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  public version: string = pack.version;
   maintenance = false;
   carouselIndex: number = 0;
   carouselData: any = [
